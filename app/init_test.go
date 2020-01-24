283abcff4365012933e4607e57aecf9bd86b0c02
thamooo <damirmagzanov@gmail.com>
2020-01-24 12:00:39 +0200

    no message

--- app/init_test.go
+++ app/init_test.go
@@ -7,6 +7,6 @@ import (
 func Test_checkAge(t *testing.T) {
     result := checkAge("2000-01-01", 20, 90)
     if result != true {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", result, true)
+       t.Errorf("Sum was incorrect, got: %v, want: %v.", result, true)
     }
 }
